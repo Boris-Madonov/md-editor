67b9e3489493a97cc4eaf7e3c367bc084ca0fc65
Boris Madonov <maboig@yandex.ru>
2021-03-25 00:00:32 +0300

    feat: реализовать функционал приложения

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import './index.css';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handlerChangeText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="page">
       <section className="md-editor__container">
-        <textarea className="md-editor__area" />
-        <ReactMarkdown className="md-editor__preview" />
+        <textarea
+          className="md-editor__area"
+          onChange={handlerChangeText}
+          value={text}
+          placeholder="Введите текст"
+        />
+        <ReactMarkdown
+          className="md-editor__preview"
+          source={text}
+        />
       </section>
     </div>
   );
